fe025345aff493c7dd00a85936a7d23df388c55f
Oleksandr <superbowl@ukr.net>
2022-09-12 20:43:17 +0300

    Update test-script.js

--- js/test-script.js
+++ js/test-script.js
@@ -5,13 +5,14 @@ form.addEventListener("submit", onSearch);
 function onSearch(e) {
   e.preventDefault();
   let query = e.target.elements.query.value.trim();
+  console.log(query);
   if (!query) {
     return;
   } else {
     fetchMovies(query)
       .then((data) => {
         console.log(data);
-        input.innerHTML = "";
+        e.target.elements.query.value = "";
       })
       .catch((error) => {
         console.log(error);
@@ -22,7 +23,7 @@ function onSearch(e) {
 function fetchMovies(query) {
   const BASE_URL = "https://api.themoviedb.org/3/search/movie";
   const API_KEY = "76ca17943e227c67de15be6c6d3599fa";
-  return fetch(`${BASE_URL}?api_key=${API_KEY}&query=${query}`).then(
+  return fetch(`${BASE_URL}?api_key=${API_KEY}&query=${query}&page=2`).then(
     (response) => {
       if (!response.ok) {
         throw new Error(response.status);
@@ -31,31 +32,3 @@ function fetchMovies(query) {
     }
   );
 }
-
-function fetchGenres() {
-  const BASE_URL = "https://api.themoviedb.org/3/genre/movie/list";
-  const API_KEY = "6308d1a98819d8ffdd4916cbcea5cd95";
-  return fetch(`${BASE_URL}?api_key=${API_KEY}`).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
-
-function searchGenreById(id) {
-  fetchGenres()
-    .then((data) => {
-      console.log(data);
-      const searchGenre = data.genres.find((e) => e.id === id);
-      console.log(searchGenre);
-      console.log("id: ", searchGenre.id);
-      console.log("name: ", searchGenre.name);
-    })
-    .catch((error) => console.log(error));
-}
-
-// fetchGenres()
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error))
-searchGenreById(99);
