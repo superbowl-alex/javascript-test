0b031272b3225fb08a1ebf594d4a052a99906de6
Oleksandr <superbowl@ukr.net>
2022-10-12 21:45:34 +0300

    Update test-script.js

--- js/test-script.js
+++ js/test-script.js
@@ -1,34 +1,18 @@
-const form = document.querySelector(".search-form");
-const input = document.querySelector(".search-form__input");
-form.addEventListener("submit", onSearch);
+fetchData()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-function onSearch(e) {
-  e.preventDefault();
-  let query = e.target.elements.query.value.trim();
-  console.log(query);
-  if (!query) {
-    return;
-  } else {
-    fetchMovies(query)
-      .then((data) => {
-        console.log(data);
-        e.target.elements.query.value = "";
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-}
-
-function fetchMovies(query) {
-  const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-  const API_KEY = "76ca17943e227c67de15be6c6d3599fa";
-  return fetch(`${BASE_URL}?api_key=${API_KEY}&query=${query}&page=2`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
+function fetchData() {
+  return fetch(
+    `https://api-2445581417326.apicast.io:443/api/v1/workouts/530?api_key: 01429dc958ed709f282318ac634b681b`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-  );
+    return response.json();
+  });
 }
