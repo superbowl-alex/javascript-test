4aae4ba97e3e5fa6f8367def87d7d75eb7690e02
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2023-01-26 10:49:14 +0200

    Open/close different popup

---
+++ js/openModal.js
@@ -0,0 +1,57 @@
+(() => {
+  const popups = [
+    {
+      button: "js-header-cart-button",
+      modal: "header-cart",
+    },
+    {
+      button: "js-header-localization-btn",
+      modal: "header__localization-popup",
+    },
+  ];
+
+  const normalizedString = (str) => {
+    return `.${str}`;
+  };
+
+  const closeModal = () => {
+    for (let popup of popups) {
+      const ref = document.querySelector(normalizedString(popup.modal));
+      ref.classList.remove("is-active");
+    }
+  };
+
+  const closeModalByEsc = (e) => {
+    if (e.code === "Escape") {
+      closeModal();
+      currentButton = null;
+    }
+  };
+
+  let currentButton = null;
+
+  const openModal = (e) => {
+    e.preventDefault();
+    closeModal();
+    if (currentButton === e.target) {
+      currentButton = null;
+      return;
+    }
+    currentButton = e.target;
+    for (let popup of popups) {
+      if (currentButton.classList.contains(popup.button)) {
+        document
+          .querySelector(normalizedString(popup.modal))
+          .classList.add("is-active");
+      }
+    }
+  };
+
+  for (let popup of popups) {
+    let ref = document.querySelector(normalizedString(popup.button));
+    ref.addEventListener("click", openModal);
+  }
+  const closeModalBtn = document.querySelectorAll(".modal-close-button");
+  closeModalBtn.forEach((item) => item.addEventListener("click", closeModal));
+  window.addEventListener("keydown", closeModalByEsc);
+})();
